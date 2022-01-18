e4085de4fcf8d636793c25033c4ef84d9f748e03
我叫大衛 <david.chirkinian@gmail.com>
2022-01-18 18:17:26 +0200

    -while-

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
     Random random = new Random();
     public static void main(String[] args) {
         new Main().run();
@@ -11,21 +10,20 @@ public class Main {
     private void run() {
         int[] mass = add(20);
         int max = mass[0];
-        for (int i = 0; i < mass.length; i++) {
+        int i = 1;
+        while (i < mass.length) {
             if (mass[i] > max) max = mass[i];
+            i++;
         }
         System.out.println(max);
     }
 
-    public int[] add( int a) {
-        a = scanner.nextInt();
-        int[] mass = new int[a];
-        int n = random.nextInt();
-        for (int i = 0; i < a; i++) {
-            System.out.println(n);
+    public int[] add( int n) {
+        int[] mass = new int[n];
+         for (int i = 0; i < n; i++) {
+           mass[i] = random.nextInt(100);
 
         }
-        return n;
         return mass;
     }
 }
